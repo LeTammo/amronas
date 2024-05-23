546d1a12d935ac7249becbec03cb1e71813fc933
Tammo <tammo@amronas.one>
2024-05-23 15:07:44 +0200

    Add logging, change cookie handling

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -12,4 +12,5 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
